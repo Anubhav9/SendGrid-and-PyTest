f4fd980fc99542c737f874c7517bd84ef5d155d7
Anubhav Sanyal <princebest3@rediffmail.com>
2023-03-05 23:17:20 +0530

    Update app_test.py

--- app_test.py
+++ app_test.py
@@ -2,7 +2,7 @@ from app import create_email
 
 def test_positive_scenario():
     response=create_email("Hello","hello@gmail.com")
-    assert response==202,"Status Code not matching"
+    assert response==401,"Status Code not matching"
 
 def test_negative_scenario_empty_receipent_email():
     response=create_email("Hello","")
