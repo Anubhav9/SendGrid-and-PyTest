d4ba86caf81d81580cdd288f68b740990c0a5536
Anubhav Sanyal <princebest3@rediffmail.com>
2023-03-05 22:28:54 +0530

    changed validation in test post removing the API key

--- app_test.py
+++ app_test.py
@@ -1,8 +1,9 @@
 from app import createEmail
 
 def test_positive_scenario():
+    ## Status Code 401 becuase API Key has been removed
     response=createEmail("Hello","hello@gmail.com")
-    assert response==202,"Status Code not matching"
+    assert response==401,"Status Code not matching"
 
 def test_negative_scenario_empty_receipent_email():
     response=createEmail("Hello","")
